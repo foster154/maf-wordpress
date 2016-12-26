ef9f6ef8c24afe8b1ff77fc418b39db5e321fa74
Mark Foster <foster154@gmail.com>
2016-12-26 10:55:20 -0700

    format front page testimonial

--- page-home.php
+++ page-home.php
@@ -11,4 +11,28 @@ get_header(); ?>
   I build websites and apps</br> that help businesses grow.
 </section>
 
+<section class="fp-testamonials clearfix">
+  <div class="fp-testamonials-wrapper">
+    <img src="<?php bloginfo('stylesheet_directory'); ?>/img/quote.svg" />
+    <div class="quote">
+      Mark really captured the vision for our organization. He is a great communicator, which made the whole process very user friendly.  I have nothing but good things to say--would definitely recommend working with Mark!
+      <span class="quote-by"><em> - Amanda, LEAP Charities</em></span>
+    </div>
+
+
+  </div>
+</section>
+
+<section class="fp-portfolio">
+  <h2>Portfolio</h2>
+</section>
+
+<section class="fp-articles">
+  <h2>Articles</h2>
+</section>
+
+<section class="about">
+  <h2>About</h2>
+</section>
+
 <?php get_footer();
